12b33e6b2e37dd1bccd24c2bd783ef26a42622f1
LAB11_USER ON WS10 <L11X16W10@citulab.net>
2023-11-22 17:56:39 +0800

    added zzPrefinal activity

---
+++ PREFINAL/zz_PREFINAL/zzForum.js
@@ -0,0 +1,8 @@
+$('#loginModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+$('#newUserModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
